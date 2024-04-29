e79610ff53a122d8daeeee519e18a96f7897e47a
Gustavo-Hofs <hofstatter.gustavo@engenharia.ufjf.br>
2024-04-29 16:32:28 -0300

    feat: func saque

--- bootcamps/Python_AI_Backend_Developer/Criando_um_Sistema_Bancário_com_Python/main.py
+++ bootcamps/Python_AI_Backend_Developer/Criando_um_Sistema_Bancário_com_Python/main.py
@@ -24,7 +24,7 @@ while True:
             valor = float(input("Digite o valor que tu deseja depositar: ")) 
             if valor >= 0:
                 saldo += valor
-                print(f"O valor foi depositado com sucesso.")
+                print("O valor foi depositado com sucesso.")
             else:
                 print("O valor precisa ser maior que  zero.")
         except:
@@ -32,6 +32,25 @@ while True:
 
     elif opcao == "s":
         print("Saque.")
+        if numero_saques < LIMITE_SAQUES:
+            try:
+                valor = float(input("Digite o valor que tu deseja sacar: ")) 
+                if valor >= 0:
+                    if valor > saldo:
+                        print("Só é possivel sacar um valor menor ou igual ao valor em saldo.")
+                    else:
+                        if valor <= LIMITE_POR_SAQUE:
+                            numero_saques += 1
+                            saldo -= valor
+                            print("O valor foi sacado com sucesso.")
+                        else:
+                            print(f"O limite máximo por saque é de R$ {LIMITE_POR_SAQUE:0.2f}.")
+                else:
+                    print("O valor precisa ser maior que  zero.")
+            except:
+                print("Entrada inválida.")
+        else:
+            print("Numero de saques diário foi excedido.")
 
     elif opcao == "e":
         print("Extrato.")
