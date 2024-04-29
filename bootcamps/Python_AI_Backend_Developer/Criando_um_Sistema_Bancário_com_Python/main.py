0dcf613cf15612a1d060ae082ef838ac2e769971
Gustavo-Hofs <hofstatter.gustavo@engenharia.ufjf.br>
2024-04-29 16:17:44 -0300

    feat: func deposito

--- bootcamps/Python_AI_Backend_Developer/Criando_um_Sistema_Bancário_com_Python/main.py
+++ bootcamps/Python_AI_Backend_Developer/Criando_um_Sistema_Bancário_com_Python/main.py
@@ -12,6 +12,7 @@ LIMITE_POR_SAQUE = 500.0
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
+valor = 0.0
 
 while True:
 
@@ -19,6 +20,15 @@ while True:
 
     if opcao == "d":
         print("Deposito.")
+        try:
+            valor = float(input("Digite o valor que tu deseja depositar: ")) 
+            if valor >= 0:
+                saldo += valor
+                print(f"O valor foi depositado com sucesso.")
+            else:
+                print("O valor precisa ser maior que  zero.")
+        except:
+            print("Entrada inválida.")
 
     elif opcao == "s":
         print("Saque.")
